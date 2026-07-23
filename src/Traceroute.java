778da8db54e76e40c049e0972cfb7c97dd5b6d05
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-07-23 21:32:00 +0000

    Fix: Traceroute class cleaned

--- src/Traceroute.java
+++ src/Traceroute.java
@@ -1,22 +1,24 @@
+
+// Importing the java dependencies
 import java.io.*;
 
+// class traceroute defintion
 public class Traceroute {
 
+    // method for tracing the destion using the processBuilder in the java .io
     public void trace(String destination)
             throws IOException {
 
-        ProcessBuilder pb =
-                new ProcessBuilder("tracert", destination);
+        ProcessBuilder pb = new ProcessBuilder("tracert", destination);
 
         Process process = pb.start();
 
-        BufferedReader reader =
-                new BufferedReader(
-                        new InputStreamReader(process.getInputStream()));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
 
         String line;
 
-        while((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
 
             System.out.println(line);
 
